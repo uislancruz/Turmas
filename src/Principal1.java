b76f1a7a915d771f757b5e700d4778855a17cacd
Uislan Cruz <uislansk8@hotmail.com>
2025-06-21 11:36:14 -0300

    ArrayList

---
+++ src/Principal1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Principal1 {
+
+    public static void main(String[] args) {
+        ArrayList<String> alunos = new ArrayList<>();
+//        alunos[0] = "Joao";
+//        alunos[1] = "Laura";
+
+        for (int i = 0; i < alunos.length; i++) {
+            String aluno = alunos[i];
+            System.out.println(aluno);
+
+        }
+    }
+}
