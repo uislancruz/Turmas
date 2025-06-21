f2fca445eecbe155790812b8cf6b0a71f5418d07
Uislan Cruz <uislansk8@hotmail.com>
2025-06-21 11:57:56 -0300

    ArrayList ajustando as classes para Arraylist

--- src/Turma.java
+++ src/Turma.java
@@ -1,22 +1,33 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Turma {
     String identificacao;
     String nomeProfessor;
-    Aluno[] alunos = new Aluno[0];
+    ArrayList<Aluno> alunos = new ArrayList<>();
 
     void adicionarAluno(Aluno aluno){
-        this.alunos = Arrays.copyOf(this.alunos, this.alunos.length + 1);
-        alunos[alunos.length -1] = aluno;
+        alunos.add(aluno);
+
+//        this.alunos = Arrays.copyOf(this.alunos, this.alunos.length + 1);
+//        alunos[alunos.length -1] = aluno;
+    }
+
+    void removerAluno(int indice){
+        alunos.remove(indice);
     }
 
     void imprimirListaDeAlunos() {
         for (Aluno aluno : alunos) {
-            if (aluno != null) {
-                System.out.printf("%s (%d anos)%n", aluno.nome, aluno.idade);
-            } else {
-                System.out.println("Vago");
-            }
+            System.out.printf("%s (%d anos)%n", aluno.nome, aluno.idade);
         }
+
+//        for (Aluno aluno : alunos) {
+//            if (aluno != null) {
+//                System.out.printf("%s (%d anos)%n", aluno.nome, aluno.idade);
+//            } else {
+//                System.out.println("Vago");
+//            }
+//        }
     }
 }
