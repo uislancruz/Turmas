c2262c8005cb6c4f8c09e0dead0bf2dda0655511
Uislan Cruz <uislansk8@hotmail.com>
2025-06-23 20:12:28 -0300

    Arrays irregular, onde consigo manipular a quantidade de arrays de cada ponto

--- src/ArraysMultidimnensionais.java
+++ src/ArraysMultidimnensionais.java
@@ -1,16 +1,18 @@
 public class ArraysMultidimnensionais {
     public static void main(String[] args) {
-        String[][] cidade = new String[3][3];
+        String[][] cidade = new String[3][];
+        cidade[0] = new String[3];
         cidade[0][0] = "Uberlandia";
         cidade[0][1] = "Uberaba";
         cidade[0][2] = "Belo Horizonte";
 
+        cidade[1] = new String[2];
         cidade[1][0] = "São Paulo";
         cidade[1][1] = "Ribeirão Preto";
 
+        cidade[2] = new String[1];
         cidade[2][0] = "Fortaleza";
 
-        System.out.println(cidade[0][1]);
 
     }
 }
